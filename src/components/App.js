1f3860b69e5620bcffaef79e9b8f14f0f57685b7
rishabhc32 <rishabhc32@gmail.com>
2019-05-31 20:31:32 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import StartPage from './StartPage';
 import Recoder from './Recorder';
+import Loader from './Loader';
 
 function App() {
     return (
         <Router>
             <Route exact path="/" component={StartPage} />
-            <Route exact path="/start" component={Recoder} />
+            <Route exact path="/start" component={Loader} />
         </Router >
     );
 }
